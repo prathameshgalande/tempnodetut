2048eceddd964b58b8ee376dbe679ce191eaf6b0
unknown <prathameshgalande7@gmail.com>
2025-03-18 09:59:29 +0530

    second commit

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-const arr = [1, [2,3], [4,5,6]]
+const http = require('http')
 
-const check = arr.flatten();
-console.log(check)
+const server = http.createServer((req,res)=>{
+    if(req.url === '/'){
+        res.end('Welcome to our Home Page');
+    }
+    if(req.url === '/about'){
+        res.end('This is a short summary of our website');
+    }
+})
+
+server.listen(3000);
+console.log("Listening to port 3000...Please go to localhost:3000 on your browser...");
